98235cd18a8787abba265bf3a5caec8669dbd76c
游家和 <47984803+usernamehehe@users.noreply.github.com>
2023-09-12 13:20:41 +0800

    Add files via upload

---
+++ PS/PSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSApp.swift
+//  PS
+//
+//  Created by 游家和 on 2023/5/12.
+//
+
+import SwiftUI
+
+@main
+struct PSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
